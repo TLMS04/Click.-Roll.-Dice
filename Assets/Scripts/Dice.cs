b928787a2c17360f769fd490c22f7a5274aa87df
TLMS04 <47973422+TLMS04@users.noreply.github.com>
2022-04-28 19:31:07 +0600

    Переписал класс Dice без switch

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -4,79 +4,25 @@ using UnityEngine;
 [Serializable]
 public enum DiceEnum
 {
-    d4,
-    d6,
-    d8,
-    d10,
-    d12,
-    d20,
-    d100
+    d4 = 4,
+    d6 = 6,
+    d8 = 8,
+    d10 = 10,
+    d12 = 12,
+    d20 = 20
 }
 public static class Dice
 {
     public static int RollDice(DiceEnum diceEnum)
     {
-        int result = 0;
-        switch (diceEnum)
-        {
-            case DiceEnum.d4:
-                result = UnityEngine.Random.Range(1, 5);
-                break;
-            case DiceEnum.d6:
-                result = UnityEngine.Random.Range(1, 7);
-                break;
-            case DiceEnum.d8:
-                result = UnityEngine.Random.Range(1, 9);
-                break;
-            case DiceEnum.d10:
-                result = UnityEngine.Random.Range(1, 11);
-                break;
-            case DiceEnum.d12:
-                result = UnityEngine.Random.Range(1, 13);
-                break;
-            case DiceEnum.d20:
-                result = UnityEngine.Random.Range(1, 21);
-                break;
-            case DiceEnum.d100:
-                result = UnityEngine.Random.Range(1, 101);
-                break;
-            default:
-                result = 0;
-                break;
-        }
-        return result;
+        return UnityEngine.Random.Range(1, (int)diceEnum + 1);
     }
     public static List<int> RollDice(DiceEnum diceEnum, uint d = 1)
     {
         List<int> result = new List<int>();
         for (int i = 0; i < d; i++)
         {
-            switch (diceEnum)
-            {
-                case DiceEnum.d4:
-                    result.Add(UnityEngine.Random.Range(1, 5));
-                    break;
-                case DiceEnum.d6:
-                    result.Add(UnityEngine.Random.Range(1, 7));
-                    break;
-                case DiceEnum.d8:
-                    result.Add(UnityEngine.Random.Range(1, 9));
-                    break;
-                case DiceEnum.d10:
-                    result.Add(UnityEngine.Random.Range(1, 11));
-                    break;
-                case DiceEnum.d12:
-                    result.Add(UnityEngine.Random.Range(1, 12));
-                    break;
-                case DiceEnum.d20:
-                    result.Add(UnityEngine.Random.Range(1, 20));
-                    break;
-                case DiceEnum.d100:
-                    result.Add(UnityEngine.Random.Range(1, 101));
-                    break;
-                default:
-                    break;
-            }
+            result.Add(UnityEngine.Random.Range(1, (int)diceEnum + 1 ));
         }
         return result;
     }
--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -12,12 +12,18 @@ public class GameUI : MonoBehaviour
 
 
     private GameData _gameData;
-
+    private Dictionary<int, Sprite> _spritesDiceDictionaty;
     void Start()
     {
+        _spritesDiceDictionaty = new Dictionary<int, Sprite>();
+        List<int> diceNumber = new List<int>() { 4, 6, 8, 10, 12, 20 }; 
+        for (int i = 0; i < _spritesDice.Count; i++)
+        {
+            _spritesDiceDictionaty.Add(diceNumber[i], _spritesDice[i]);
+        }
         _gameData = GameData.GetInstance();
         Game.ScoreChanged.AddListener(OnScoreChanged);
-        _tabZone.GetComponent<SpriteRenderer>().sprite = _spritesDice[(int)_gameData.Dices[0]];
+        _tabZone.GetComponent<SpriteRenderer>().sprite = _spritesDiceDictionaty[(int)_gameData.Dices[0]];
     }
 
     public void OnScoreChanged(long score)
